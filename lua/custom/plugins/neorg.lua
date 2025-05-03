2f934d4f28f0ad899ddfb98efc15ebb3faf47f67
mini <mini@newtown>
2025-05-03 09:51:44 +0800

    modify neorg

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -1,6 +1,32 @@
 return {
-    "nvim-neorg/neorg",
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = "*", -- Pin Neorg to the latest stable release
-    config = true,
+  {
+    'rebelot/kanagawa.nvim', -- neorg needs a colorscheme with treesitter support
+    config = function()
+      vim.cmd.colorscheme 'kanagawa'
+    end,
+  },
+  {
+    'nvim-neorg/neorg',
+    lazy = false,
+    version = '*',
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {},
+          ['core.concealer'] = {},
+          ['core.dirman'] = {
+            config = {
+              workspaces = {
+                notes = '~/notes',
+              },
+              default_workspace = 'notes',
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
 }
