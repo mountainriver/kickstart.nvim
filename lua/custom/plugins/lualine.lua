573cf65dfedd39eaa23951c817c8bc28988f75a9
Forest <forest@solo.com>
2025-06-04 17:46:00 +0800

    x

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'codedark',
+        theme = 'gruvbox-material',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
